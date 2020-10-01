f8a003b8c20ad89b83364b9e4447d5f0b75d47b1
pandracoon <whworjs7946@naver.com>
2020-10-01 23:59:01 +0900

    kakaoMap added?

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -5,3 +5,11 @@ function openNav() {
 function closeNav() {
     document.getElementById("menu").style.width = "0";
 }
+
+var container = document.getElementById('map');
+var options = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    level: 3
+}
+
+var map = new kakao.maps.Map(container, options);
